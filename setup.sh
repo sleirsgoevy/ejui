2db08e826ca11946dc28425a6c3fe62ac013909d
sleirsgoevy <sleirsgoevy@gmail.com>
2019-02-21 22:30:25 +0300

    Fix setup script (not git-cloning anymore)

--- setup.sh
+++ setup.sh
@@ -1,19 +1,17 @@
 SERVER="$1"
 
-wget -r "http://$SERVER:8000/.git"
-cd "$SERVER:8000"
-git checkout master
-git stash
-
+wget "http://$1:8000/ejui.tar.gz" -O - | tar -xvzf -
 bash download_brutejudge.sh
 
-cat > ../ejui.sh << EOF
+sudo pip3 install bottle
+
+cat > ejui.sh << EOF
 #!/bin/bash
 
 SERVER=your-server-url # for jjs: http://127.0.0.1:1779/
 BIND_ADDR=127.0.0.1:8080 # or any other port
 
-cd "\$(dirname "\$0")/$SERVER:8000"
+cd "\$(dirname "\$0")/ejui"
 python3 ejui.py "\$BIND_ADDR" "\$SERVER"
 EOF
 
---
+++ web-install.py
@@ -0,0 +1,23 @@
+import http.server, tarfile, gzip, io
+
+class Handler(http.server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            data = open('setup.sh', 'rb').read()
+        elif self.path == '/ejui.tar.gz':
+            i = io.BytesIO()
+            j = gzip.GzipFile('ejui.tar.gz', 'w', fileobj=i)
+            k = tarfile.TarFile('ejui.tar', 'w', fileobj=j)
+            k.add('.', 'ejui')
+            k.close()
+            j.close()
+            data = i.getvalue()
+        else:
+            self.send_error(404)
+            return
+        self.send_response(200)
+        self.send_header('Content-Length', str(len(data)))
+        self.end_headers()
+        self.wfile.write(data)
+
+http.server.HTTPServer(('127.0.0.1', 8000), Handler).serve_forever()
