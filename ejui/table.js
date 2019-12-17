6f99dc43d3d276af0134a0693093cf794b4519fb
Sergey Lisov <sleirsgoevy@gmail.com>
2019-12-17 14:05:37 +0300

    blablabla

---
+++ ejui/table.js
@@ -0,0 +1,215 @@
+function AnimatedTable(cols)
+{
+    this.cols = cols;
+    this.colsSet = {};
+    for(var i = 0; i < this.cols.length; i++)
+    {
+        this.cols[i]._refcount = 0;
+        this.colsSet[this.cols[i].id] = true;
+    }
+    this.theTable = document.createElement('table');
+    this.theTH = document.createElement('tr');
+    this.theTable.appendChild(this.theTH);
+    this.rows = [];
+    this.animating = false;
+}
+
+AnimatedTable.prototype.insertColumn = function(q, skip_row)
+{
+    if(!this.colsSet[q])
+        throw new ReferenceError("wtf?");
+    var idx = 0, i;
+    for(i = 0; this.cols[i].id != q; i++) //SQUARE
+        if(this.cols[i]._refcount > 0 || this.cols[i]._opacity !== undefined)
+            idx++;
+    var thNode = document.createElement('th');
+    thNode.appendChild(document.createTextNode(this.cols[i].name));
+    if(idx == this.theTH.length)
+        this.theTH.appendChild(thNode);
+    else
+        this.theTH.insertBefore(thNode, this.theTH.childNodes[idx]);
+    for(var j = 0; j < this.rows.length; j++)
+    {
+        var text = null;
+        if(this.rows[j][q] !== undefined && this.rows[j].id !== skip_row)
+            text = this.rows[j][q];
+        var cell = document.createElement('td');
+        if(text !== null)
+            cell.appendChild(document.createTextNode(text));
+        var row = this.rows[j]._tr;
+        if(idx == row.childNodes.length)
+            row.appendChild(cell);
+        else
+            row.insertBefore(cell, row.childNodes[idx]);
+    }
+    this.cols[i]._opacity = 0;
+    if(!this.animating)
+        this.animate();
+}
+
+AnimatedTable.prototype.removeColumn = function(q)
+{
+    if(!this.colsSet[q])
+        throw new ReferenceError("wtf?");
+    var idx = 0, i;
+    for(i = 0; this.cols[i].id != q; i++) //SQUARE
+        if(this.cols[i]._refcount > 0 || this.cols[i]._opacity !== undefined)
+            idx++;
+    this.theTH.removeChild(this.theTH.childNodes[idx]);
+    for(var j = 0; j < this.rows.length; j++)
+        this.rows[j]._tr.removeChild(this.rows[j]._tr.childNodes[idx]);
+}
+
+AnimatedTable.prototype.increfColumn = function(q, skip_row)
+{
+    if(!this.colsSet[q])
+        throw new ReferenceError("wtf?");
+    var i;
+    for(i = 0; this.cols[i].id != q; i++); //SQUARE
+    if(!(this.cols[i]._refcount++))
+        this.insertColumn(q, skip_row);
+}
+
+AnimatedTable.prototype.decrefColumn = function(q)
+{
+    if(!this.colsSet[q])
+        throw new ReferenceError("wtf?");
+    var i;
+    for(i = 0; this.cols[i].id != q; i++); //SQUARE
+    if(!--this.cols[i]._refcount)
+    {
+        this.cols[i]._opacity = 1;
+        if(!this.animating)
+            this.animate();
+    }
+}
+
+AnimatedTable.prototype.insertRow = function(r)
+{
+    var i;
+    for(i = 0; i < this.rows.length && this.rows[i].id < r; i++); //SQUARE
+    this.rows.splice(i, 0, r);
+    r._tr = document.createElement('tr');
+    for(var j = 0; j < this.cols.length; j++)
+        if(this.cols[j]._refcount > 0 || this.cols[j]._opacity > 0)
+            r.appendChild(document.createElement('tr'));
+    var tbody = this.theTH.parentNode;
+    i++;
+    if(i == tbody.childNodes.length)
+        tbody.appendChild(r._tr);
+    else
+        tbody.insertBefore(r._tr, tbody.childNodes[i]);
+    r._opacity = 0;
+    if(!this.animating)
+        this.animate();
+}
+
+AnimatedTable.prototype.updateRow = function(r)
+{
+    if(!r._tr)
+        this.insertRow(r);
+    r._toremove = false;
+    for(var i = 0; i < this.cols.length; i++)
+        if(this.cols[i].id in r)
+            this.increfColumn(this.cols[i].id, r.id);
+    var idx = 0;
+    for(var i = 0; i < this.cols.length; i++)
+        if(this.cols[i]._refcount > 0 || this.cols[i]._opacity !== undefined)
+            if(r._tr.childNodes[idx++].childNodes.length)
+                this.decrefColumn(this.cols[i].id);
+    idx = 0;
+    for(var i = 0; i < this.cols.length; i++)
+        if(this.cols[i]._refcount > 0 || this.cols[i]._opacity !== undefined)
+        {
+            var td = r._tr.childNodes[idx++];
+            var data = r[this.cols[i].id];
+            while(td.firstChild)
+                td.removeChild(td.firstChild);
+            if(data !== null && data !== undefined)
+                td.appendChild(document.createTextNode(data));
+        }
+}
+
+AnimatedTable.prototype.removeRow = function(r)
+{
+    r._toremove = true;
+    if(r._opacity === undefined)
+        r._opacity = 1;
+    if(!this.animating)
+        this.animate();
+}
+
+AnimatedTable.prototype.animate = function()
+{
+    this.animating = true;
+    var dirty = false;
+    for(var i = 0; i < this.rows.length; i++) //SQUARE
+        if(this.rows[i]._opacity !== undefined)
+            for(var j = 0; j < this.rows[i]._tr.childNodes.length; j++)
+                this.rows[i]._tr.childNodes[j].style.opacity = this.rows[i]._opacity;
+    var idx = 0;
+    for(var i = 0; i < this.cols[i].length; i++)
+    {
+        if(this.cols[i]._opacity !== undefined)
+        {
+            this.theTH.childNodes[idx].style.opacity = this.cols[i]._opacity;
+            for(var j = 0; j < this.rows.length; j++)
+            {
+                var op = this.cols[i]._opacity;
+                if(this.rows[j]._opacity !== undefined && this.rows[j]._opacity < op)
+                    op = this.rows[j]._opacity;
+                this.rows[j]._tr.childNodes[idx].style.opacity = op;
+            }
+        }
+    }
+    for(var i = 0; i < this.rows.length; i++) //SQUARE
+        if(this.rows[i]._opacity !== undefined)
+        {
+            this.rows[i]._opacity = (this.rows[i]._toremove?animateRemove:animateInsert)(this.rows[i]._opacity);
+            if(this.rows[i]._opacity === undefined && this.rows[i]._toremove)
+            {
+                this.rows[i]._onremove();
+                this.rows[i]._tr.parentNode.removeChild(this.rows[i]._tr);
+                this.rows.splice(i--, 1);
+            }
+            if(this.rows[i]._opacity !== undefined)
+                dirty = true;
+        }
+    for(var i = 0; i < this.cols.length; i++) //SQUARE
+        if(this.cols[i]._opacity !== undefined)
+        {
+            this.cols[i]._opacity = (this.cols[i]._refcount?animateInsert:animateRemove)(this.cols[i]._opacity);
+            if(this.cols[i]._opacity === undefined && this.cols[i]._refcount == 0)
+                this.removeColumn(this.cols[i].id);
+            if(this.cols[i]._opacity !== undefined)
+                dirty = true;
+        }
+    if(dirty)
+        requestAnimationFrame(this.animate.bind(this));
+    else
+        this.animating = false;
+}
+
+function animateInsert(opacity)
+{
+    if(opacity === 1)
+        return undefined;
+    opacity = Math.pow(opacity, 1/3);
+    opacity += 1/60;
+    opacity = Math.pow(opacity, 3);
+    if(opacity > 1)
+        opacity = 1;
+    return opacity;
+}
+
+function animateRemove(opacity)
+{
+    if(opacity === 0)
+        return undefined;
+    opacity = Math.pow(opacity, 1/3);
+    opacity -= 1/30;
+    opacity = Math.pow(opacity, 3);
+    if(opacity < 0)
+        opacity = 0;
+    return opacity;
+}
